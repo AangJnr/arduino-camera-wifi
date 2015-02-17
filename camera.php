4a962e94a29226586b3ccc199f21cef47de6db4f
Aether <aether.wu@gmail.com>
2015-02-16 22:23:27 -0500

    Update camera.php
    Made the feedback simpler so that you can see the result by byte number in Apache's access_log. 
    > cat /var/log/apache2/access_log 
    on my Mac OS Yosemite. 
    You should see "POST /camera.php HTTP/1.1" 200" with one more number at the end. 
    If the number is 1, then the photo was uploaded successful.

--- camera.php
+++ camera.php
@@ -1,16 +1,12 @@
 <?php
 
- $target_path = "uploads/";
+$target_path = "uploads/";
 
 $target_path = $target_path . basename( $_FILES['picture']['name']); 
 
 if(move_uploaded_file($_FILES['picture']['tmp_name'], $target_path)) {
-    echo "The file ".  basename( $_FILES['picture']['name']). 
-    " has been uploaded";
-
-} 
-else {
-    echo "There was an error uploading the file, please try again!";
+    echo "1";
 }
 
+print_r($_FILES);
 ?>
